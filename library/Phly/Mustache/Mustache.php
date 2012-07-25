ae5e50f5613449782e56dd0b30f04e9e0eb60615
a.thompson2@comicrelief.com <a.thompson2@comicrelief.com@ee59469e-3052-11e5-ab9d-cde3995dece7>
2012-07-25 12:18:01 +0000

    Remove strict array type checks and functions for cachedTemplate.

--- library/Phly/Mustache/Mustache.php
+++ library/Phly/Mustache/Mustache.php
@@ -202,7 +202,7 @@ class Mustache
             return $lexer->compile($template);
         }
 
-        if (array_key_exists($template, $this->cachedTemplates)
+        if (isset($this->cachedTemplates[$template])
             && is_array($this->cachedTemplates[$template])
         ) {
             return $this->cachedTemplates[$template];
@@ -239,7 +239,7 @@ class Mustache
      * @param  array $tokens 
      * @return Mustache
      */
-    public function restoreTokens(array $tokens)
+    public function restoreTokens($tokens)
     {
         $this->cachedTemplates = $tokens;
         return $this;
